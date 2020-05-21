68d0f3f7870b8d6550472826ff05ec6c7aa40201
Ethan Hong <20248019+ethanhong@users.noreply.github.com>
2020-05-21 02:31:00 -0700

    move parameters and constants together in this file

--- parameter.py
+++ parameter.py
@@ -1,4 +1,19 @@
+# Account info
+troop_slot = 3
 
-DELAY_BETWEEN_CLICKS = 1.00
 
-TROOP_SLOT = 3
+# Layout setting
+# this is the actual window size when robot is running,
+# set it equal to GAME_BASE_SIZE when you are making needles
+game_window_size = (600, 1105)
+win_title = ['BlueStacks']
+
+
+# Robot setting
+delay_between_clicks = 1.00  # set higher when the game does not run smoothly
+img_match_confidence = 0.8
+
+
+# Constants
+GAME_BASE_SIZE = (600, 1105)  # this is the window size where needles made from
+BANNER_H = 37  # upper banner height of BlueStacks window
