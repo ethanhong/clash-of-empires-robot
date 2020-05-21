4af99812a05848758b937cda1ac5016057e09818
Ethan Hong <20248019+ethanhong@users.noreply.github.com>
2020-05-21 02:36:15 -0700

    rewrite class Button for relative coordinate
    now the coordinate clicks and neeedle matchs can work normally even the window is moved or size changed

--- core.py
+++ core.py
@@ -1,7 +1,12 @@
-from recovery import recovery
-from utility import *
+import os
+from time import sleep, perf_counter
 
-GAME_WINDOW_SIZE = (600, 1105)
+import PIL
+import pyautogui
+import pygetwindow as gw
+
+from parameter import *
+from recovery import recovery
 
 
 class Screen:
@@ -20,6 +25,50 @@ class ResType:
     WOOD = 3
     IRON = 4
     SILVER = 5
+
+
+class Button:
+    def __init__(self, img, center):
+        x, y = center
+        self.center = x, y
+        self.img = img
+
+    def __getitem__(self, item):
+        if item == 0:
+            return self.img
+        elif item == 1:
+            return abs_position(self.center)
+
+    def __repr__(self):
+        return str([self.img, abs_position(self.center)])
+
+    def click(self, times=1):
+        for _ in range(times):
+            pyautogui.click(abs_position(self.center))
+            sleep(delay_between_clicks)
+
+    def visible(self):
+        filename = img_path(self.img)
+        im = PIL.Image.open(filename)
+        ndl = resize_by_window(im)
+        haystack = self._haystack()
+        return bool(pyautogui.locate(ndl, haystack, confidence=img_match_confidence))
+
+    def visible_in(self, area):
+        filename = img_path(self.img)
+        im = PIL.Image.open(filename)
+        ndl = resize_by_window(im)
+        haystack = pyautogui.screenshot().crop(area)
+        return bool(pyautogui.locate(ndl, haystack, confidence=img_match_confidence))
+
+    def _haystack(self):
+        filename = img_path(self.img)
+        im = PIL.Image.open(filename)
+        w, h = round(im.size[0] / 2) + 3, round(im.size[1] / 2) + 3
+        im = pyautogui.screenshot()
+        abs_center = abs_position(self.center)
+        return im.crop((abs_center[0] - w, abs_center[1] - h,
+                        abs_center[0] + w, abs_center[1] + h))
 
 
 # buttons
@@ -46,8 +95,28 @@ slv_mine = Button('', (540, 820))
 res_coord = [monster, camp, farm, sawmill, iron_mine, slv_mine]
 
 # haystack areas
-game_screen = (0, 0, GAME_WINDOW_SIZE[0], GAME_WINDOW_SIZE[1])
 prompt_msg = (85, 450, 517, 690)
+
+
+def window_pos_ratio():
+    wnd = gw.getWindowsWithTitle(win_title[0])[0]
+    width, height = wnd.size
+    x, y = wnd.topleft
+    return x, y, width / GAME_BASE_SIZE[0], (height - BANNER_H) / (GAME_BASE_SIZE[1] - BANNER_H)
+
+
+def abs_position(pos):
+    dx, dy, rx, ry = window_pos_ratio()
+    return pos[0] * rx + dx, (pos[1] - BANNER_H) * ry + BANNER_H + dy
+
+
+def resize_by_window(img):
+    wnd = gw.getWindowsWithTitle(win_title[0])[0]
+    width, height = wnd.size
+    w, h = img.size
+    new_im = img.resize((round(w * width / GAME_BASE_SIZE[0]),
+                         round(h * height / GAME_BASE_SIZE[1])))
+    return new_im
 
 
 def img_path(filename):
@@ -89,29 +158,51 @@ def countdown_timer(secs):
     print("..GO!")
 
 
-def pyautogui_init():
-    log('Initialize PyAutoGui')
-    pyautogui.FAILSAFE = True
-
-
 def game_init():
     log('Initialize game window')
-    import win32gui
-    import win32con
-    hwnd = win32gui.FindWindow(None, 'BlueStacks')
-    win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
-    win32gui.SetForegroundWindow(hwnd)
-    win32gui.MoveWindow(hwnd, 0, 0, GAME_WINDOW_SIZE[0], GAME_WINDOW_SIZE[1], True)
+    bs = gw.getWindowsWithTitle(win_title[0])[0]
+    bs.restore()
+    bs.moveTo(0, 0)
+    bs.resizeTo(game_window_size[0], game_window_size[1])
 
 
 def ally_need_help():
-    return ally_help.is_visible()
+    return ally_help.visible()
 
 
 def help_ally():
     ally_help.click()
 
 
+# def update_troop_status():
+#     troop_info_area = [(10, 200, 37, 227),
+#                        (10, 244, 37, 271),
+#                        (10, 288, 37, 315)]
+#     ts_images = {'back': 'ts_back.png',
+#                  'enemy_atk': 'ts_enemy_atk.png',
+#                  'gathering': 'ts_gathering.png',
+#                  'monster_atk': 'ts_monster_atk.png',
+#                  'scouting': 'ts_scouting.png',
+#                  'transfer': 'ts_transfer.png',
+#                  'reinforce': 'ts_reinforce.png',
+#                  'rally': 'ts_rally.png'
+#                  }
+#     result = []
+#     # confirm in kingdom screen
+#     if (castle.visible() and not desert_camp.visible()) \
+#             or search.visible():
+#         im = pyautogui.screenshot()
+#         for area in troop_info_area:
+#             for status, img in ts_images.items():
+#                 try:
+#                     if pyautogui.locate(img_path(img), im.crop(area), confidence=0.9):
+#                         result.append(status)
+#                         break
+#                 except IOError:
+#                     log('File is missing:', img_path(img))
+#         return result
+#     else:
+#         pass
 def update_troop_status():
     troop_info_area = [(10, 200, 37, 227),
                        (10, 244, 37, 271),
@@ -127,13 +218,24 @@ def update_troop_status():
                  }
     result = []
     # confirm in kingdom screen
-    if (castle.is_visible() and not desert_camp.is_visible()) \
-            or search.is_visible():
-        im = pyautogui.screenshot()
+    if (castle.visible() and not desert_camp.visible()) \
+            or search.visible():
+
         for area in troop_info_area:
+            x1, y1, x2, y2 = area
+            dx, dy, rx, ry = window_pos_ratio()
+
+            x1 = x1 * rx + dx
+            y1 = (y1 - BANNER_H) * ry + BANNER_H + dy
+            x2 = x2 * rx + dx
+            y2 = (y2 - BANNER_H) * ry + BANNER_H + dy
+
+            haystack = pyautogui.screenshot().crop((x1, y1, x2, y2))
             for status, img in ts_images.items():
                 try:
-                    if pyautogui.locate(img_path(img), im.crop(area), confidence=0.9):
+                    im = PIL.Image.open(img_path(img))
+                    ndl = resize_by_window(im)
+                    if pyautogui.locate(ndl, haystack, confidence=img_match_confidence):
                         result.append(status)
                         break
                 except IOError:
@@ -152,14 +254,14 @@ def get_screen():
     }
     result = Screen.UNKNOWN
     for btn, srn in buttons.items():
-        if btn.is_visible():
+        if btn.visible():
             result = srn
             break
     return result
 
 
 def go_kingdom():
-    while back.is_visible():
+    while back.visible():
         back.click()
         sleep(1)
     try:
